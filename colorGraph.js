dd8b01a2697bc7c0f2162f5cd83bba897bf6c0fe
Steven Lee <stevenyslee@gmail.com>
2018-12-03 17:41:59 -0800

    Complete colorGraph

--- colorGraph.js
+++ colorGraph.js
@@ -1,1 +1,19 @@
 // Given an undirected graph ↴ with maximum degree ↴ DD, find a graph coloring ↴ using at most D+1D+1 colors.
+
+function colorGraph(graph, colors) {
+  graph.forEach((node) => {
+    let illegalColors = new Set();
+    node.neighbors.forEach((color) => {
+      if (color !== null) {
+        illegalColors.add(color);
+      }
+    });
+    for (let i = 0; i < colors.length; i++) {
+      if (!illegalColors.has(colors[i])) {
+        node.color = colors[i];
+        break;
+      }
+    }
+  });
+  return graph;
+}
