1a1cb65ea86a0b63ac4e0a63a34edc2c5ef74b1c
Steven Lee <stevenyslee@gmail.com>
2018-12-10 22:19:16 -0800

    getPermutations

---
+++ getPermutations.js
@@ -0,0 +1,77 @@
+// Recursive Solution
+function getPermutations(string) {
+  let permutations = new Set();
+  const helper = (word, letters) => {
+    if (!letters) {
+      permutations.add(word);
+      return;
+    }
+    for (let i = 0; i < letters.length; i++) {
+      helper(word + letters[i], letters.slice(0, i) + letters.slice(i + 1));
+    }
+    return;
+  }
+  helper('', string);
+  return permutations;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'empty string';
+let input = '';
+let actual = getPermutations(input);
+let expected = new Set(['']);
+assert(isSetsEqual(actual, expected), desc);
+
+desc = 'one character string';
+input = 'a';
+actual = getPermutations(input);
+expected = new Set(['a']);
+assert(isSetsEqual(actual, expected), desc);
+
+desc = 'two character string';
+input = 'ab';
+actual = getPermutations(input);
+expected = new Set(['ab', 'ba']);
+assert(isSetsEqual(actual, expected), desc);
+
+desc = 'three character string';
+input = 'abc';
+actual = getPermutations(input);
+expected = new Set(['abc', 'acb', 'bac', 'bca', 'cab', 'cba']);
+assert(isSetsEqual(actual, expected), desc);
+
+function isSetsEqual(as, bs) {
+  if (as.size !== bs.size) {
+    return false;
+  }
+  for (let a of as) {
+    if (!bs.has(a)) return false;
+  }
+  return true;
+}
+
+function assert(condition, desc) {
+  if (condition) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL`);
+  }
+}
